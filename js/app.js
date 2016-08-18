cd8f122d4391ad8bc221250fadc87e997c8bf535
Kirill Stytsenko <kirill@styts.com>
2016-08-18 19:31:52 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 var css = require('../styles/main.scss');
+
+var map = L.map('map').setView([48.226016394414145, 16.50457620620728], 16);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+maxZoom: 20,
+id: 'mapbox.streets',
+accessToken: 'pk.eyJ1Ijoic3R5dHMiLCJhIjoiY2lzMGkzbHpmMDA1ajJzbzcxYXNvM2VidiJ9.ozNnfh0l7gHgBxrbovRcAw'
+}).addTo(map);
+
+var sidebar = L.control.sidebar('sidebar').addTo(map);
+sidebar.open('home');

9930fc81612aa7481937dda1d6b6f168decd9e87
Kirill Stytsenko <kirill@styts.com>
2016-08-19 22:41:46 +0200

    Better admin listing

--- server/geruch/admin.py
+++ server/geruch/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('time_added', 'name', 'floor', 'message')
+
+admin.site.register(Report, ReportAdmin)
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,22 +1,14 @@
-"""server URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('')
+
 
 urlpatterns = [
+    url(r'^$', home),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/report/', include('geruch.urls'))
+    url(r'^smell/', include('geruch.urls'))
 ]

45e854979191de2e085d36a0fae01971c0aa9277
Kirill Stytsenko <kirill@styts.com>
2016-08-22 17:23:03 +0200

    Better markers

--- js/app.js
+++ js/app.js
@@ -68,9 +68,10 @@ $('#btn-report').click(function (event) {
     },
     data: form.serialize(),
     type: 'POST',
-    success: function() {
+    success: function(data) {
       $('#notify-success').show();
       $('#message').val('');
+      markers.add_marker(data.object);
       hideAfter($('#notify-success'), 5000);
     },
     error: function() { $('#notify-failiure').show(); },
@@ -86,7 +87,7 @@ $('#reportform').submit(function (ev) {
 // update time
 function timeout() {
     setTimeout(function () {
-        $('#time').html(moment().locale('de').format('MMMM Do YYYY, h:mm:ss a'));
+        $('#time').html(moment().locale('de').format('Do MMMM YYYY, HH:mm:ss'));
         $('#time_user').val(moment().format('X'));
         if (lc._marker) {
             $.each(map._layers, function (ml) {
--- js/markers.js
+++ js/markers.js
@@ -1,4 +1,5 @@
 var format = require("string-template");
+var moment = require("moment");
 
 
 function init() {
@@ -9,27 +10,28 @@ function init() {
 
 
 function html_marker(marker) {
-  console.log(marker); 
-  return format('<div class="popup">{message}</div>', marker);
+  marker.time_ago = moment(marker.time_added).locale('de').fromNow();
+  marker.time = moment(marker.time_added).locale('de').format('Do MMMM YYYY, HH:mm');
+  return format('<div class="popup"><p>{message}</p><p>{time} (<i>{time_ago}</i>)</p></div>', marker);
 }
 
+function add_marker(marker){
+  coords = L.latLng(parseFloat(marker.latitude), parseFloat(marker.longitude));
+  mrk = new L.marker(coords)
+    .bindPopup(html_marker(marker))
+    .addTo(map);
+}
 
 function add_markers() {
   $.ajax({
     url: API_URL,
     success: function(data) {
-      var markers = data;
-      for (var i in markers) {
-        var marker = markers[i];
-        coords = L.latLng(parseFloat(marker.latitude), parseFloat(marker.longitude));
-        mrk = new L.marker(coords)
-          .bindPopup(html_marker(marker))
-          .addTo(map);
-      }
+      data.forEach(add_marker);
     }
 });
 }
 
 module.exports = {
-  init: init
+  init: init,
+  add_marker: add_marker,
 };
